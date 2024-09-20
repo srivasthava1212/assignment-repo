495ce8648aa1a9c262510198cb31ece587770f44
SRIVASTHAVA M <srivats1212@gmail.com>
2024-09-20 23:36:13 +0530

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,9 @@
+echo "Enter Principal Amount: "
+read principal
+echo "Enter Rate of Interest: "
+read rate
+echo "Enter Time (years): "
+read time
 
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+echo "Simple Interest is: $simple_interest"
